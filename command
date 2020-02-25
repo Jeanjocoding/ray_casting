3d38d50a190a7918b0a88b658d569246e7d90108
Macbook <theophile.lucille@gmail.com>
2020-02-25 20:58:14 +0100

    cub3d me voila

---
+++ command
@@ -0,0 +1,1 @@
+gcc -L minilibx_opengl_20191021 -I minilibx_opengl_20191021 -lmlx -framework OpenGL -framework appkit mlx_start.c
---
+++ mlx_start.c
@@ -0,0 +1,12 @@
+#include "minilibx_opengl_20191021/mlx.h"
+
+int	main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "test");
+	mlx_loop(mlx_ptr);
+}
+
