11684da474aeabd03f98a7e3f5fe9761986019d7
Macbook <theophile.lucille@gmail.com>
2020-05-20 11:32:20 +0200

    on avance a tatons

--- srcs/fill_int_map.c
+++ srcs/fill_int_map.c
@@ -1,10 +1,20 @@
 #include "cub3d.h"
+
 extern int **g_worldmap;
 extern int g_mlen;
 extern int g_mheight;
 extern int g_pos_check;
 
-int	 fill_map(char **tab, t_fov *fov)
+int		put_the_cell(char c, int xm, int ym, t_fov *fov)
+{
+	if (ym == 0 || ym == g_mheight)
+		get_right_char_first(c, xm, ym);
+	else
+		get_right_char_mid(c, xm, ym, fov);
+	return (0);
+}
+
+int		fill_map(char **tab, t_fov *fov)
 {
 	int xm;
 	int ym;
@@ -13,31 +23,22 @@ int	 fill_map(char **tab, t_fov *fov)
 	int len_line;
 
 	xm = 0;
-	ym = 0;
-	xt = g_mheight -1;
+	ym = -1;
+	xt = g_mheight - 1;
 	yt = 0;
-	while (ym < g_mheight)
+	while (++ym < g_mheight)
 	{
 		while (xm < g_mlen)
 		{
-//			ft_printf("tab[%d] : %s\n", xt, tab[xt]);
 			len_line = ft_strlen(tab[xt]);
 			if (yt < len_line)
-			{
-				if (ym == 0 || ym == g_mheight)
-					get_right_char_first(tab[xt][yt], xm, ym);
-				else
-					get_right_char_mid(tab[xt][yt], xm, ym, fov);
-			}
+				put_the_cell(tab[xt][yt], xm, ym, fov);
 			xm++;
 			yt++;
 		}
-//		print_map_ptr(g_worldmap, g_mlen, g_mheight);
 		xm = 0;
 		yt = 0;
-		ym++;
 		xt--;
 	}
 	return (0);
-	print_map_ptr(g_worldmap, g_mlen, g_mheight);
 }
